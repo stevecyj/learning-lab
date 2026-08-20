7a4bd90c11d396a12f729f7f64eaa7864d93fc16
stevecyj <ycsteven.tsao@gmail.com>
2026-08-21 06:51:22 +0800

    docs(sql): 新增排序、連接與資料表結構範例

--- notes/SQL的50道練習/CH1/50-1.sql
+++ notes/SQL的50道練習/CH1/50-1.sql
@@ -13,3 +13,20 @@ limit 5;
 select *
 from geographics
 limit 5;
+
+-- 排序
+select title,
+       runtime
+from movies
+order by runtime;
+
+-- 連接鍵與主鍵相連接
+select movies.title, release_info.country, release_info.released_on
+from release_info
+         join movies on release_info.movie_id = movies.id
+where movies.id = 1
+  and release_info.country = 'Taiwan';
+
+-- meta data
+select *
+from pragma_table_info('movies');
