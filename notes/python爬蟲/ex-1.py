8f075f28c8c0abd0b489e48b7dec0b27d6ef07a1
stevecyj <ycsteven.tsao@gmail.com>
2026-08-22 20:04:51 +0800

    docs(python): 補充 BeautifulSoup 連結解析範例
      - 取得 action bar 的連結文字與 href
      - 使用 urljoin 組合完整網址

--- notes/python爬蟲/ex-1.py
+++ notes/python爬蟲/ex-1.py
@@ -1,4 +1,6 @@
+from pprint import pp, pprint
 from urllib import request
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
@@ -17,5 +19,14 @@ print("狀態碼：", res.status)
 # 另一個選擇是內建的 "html.parser"，零依賴但慢一點。
 # res 是 file-like 物件，bs4 會自己 read() 並判斷編碼，所以不用先 decode。
 soup = BeautifulSoup(res, "lxml")
-print(soup)
-print(soup.title)
+action_bar = soup.find_all("div", {"id": "action-bar-container"})
+tmp_div = action_bar[0].find("div")
+tmp_a = action_bar[0].find("a")
+assert tmp_a is not None
+
+tmp_text_in_a = tmp_a.text
+tmp_url = tmp_a["href"]
+full_url = urljoin(res.url, tmp_url)
+
+# print(dir(tmp_a))
+print(full_url)
